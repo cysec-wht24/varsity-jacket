56b075229a0e3ca17245b22461db742267912967
Divyanshu <131642994+divyanshu3234@users.noreply.github.com>
2026-03-21 15:01:47 +0000

    fix double router issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -6,8 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/varsity-jacket/">
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
